01beea4ab0ee67f5a91a7cc5f56ba005c88deae8
Alvin-Tulud <alvintulud@gmail.com>
2024-07-22 23:35:38 -0700

    toggle npc dialogue

---
+++ SonicGoesToTheMoon/Assets/Scripts/Characters/Dialogue/ClickInteract.cs
@@ -0,0 +1,28 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickInteract : MonoBehaviour
+{
+    Collider2D interactbox;
+    Vector2 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactbox = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (interactbox.OverlapPoint(mousePos) && Input.GetMouseButtonDown(0))
+        {
+            //stop player movement and enable the dialogue box
+            FindAnyObjectByType<AIPath>().canMove = false;
+
+            GameObject.FindWithTag("DialogueBox").transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
