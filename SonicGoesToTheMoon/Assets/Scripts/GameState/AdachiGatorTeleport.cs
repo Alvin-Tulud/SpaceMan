d3796e6d3953fff480434d603d4c08b4637a3569
alee84sf <alee84@sfsu.edu>
2024-07-27 08:53:31 -0700

    haaland teleport

--- SonicGoesToTheMoon/Assets/Scripts/GameState/AdachiGatorTeleport.cs
+++ SonicGoesToTheMoon/Assets/Scripts/GameState/AdachiGatorTeleport.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class AdachiGatorTeleport : MonoBehaviour
 {
     public DoDialogue dialogue;
+
+    Vector3 v;
     // Start is called before the first frame update
     void Start()
     {
         //dialogue is set in the inspector
-        Vector3 v;
     }
 
     // Update is called once per frame
@@ -19,16 +20,18 @@ public class AdachiGatorTeleport : MonoBehaviour
         {
             if(gameObject.name.CompareTo("Cool Gator") == 0)
             {
-                v = new Vector3()
+                v = new Vector3(-1.4f, 1.25f, 0);
             }
             else if (gameObject.name.CompareTo("Eric Haaland") == 0)
             {
-
+                v = new Vector3(21.51f, 8.6f, 0);
             }
             else if(gameObject.name.CompareTo("Adachi") == 0)
             {
-
+                v = new Vector3(24.46f, 8.58f, 0);
             }
+
+            gameObject.transform.position = v;
         }
     }
 }
